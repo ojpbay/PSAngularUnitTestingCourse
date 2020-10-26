e4ecc0144402eccbc13f0fb5f37a58dc42c051aa
Oliver Baylis <oliver.baylis@willistowerswatson.com>
2020-10-26 18:02:48 +0000

    Tests for a service with no dependencies

---
+++ src/app/message.service.spec.ts
@@ -0,0 +1,31 @@
+import { MessageService } from "./message.service";
+
+describe("MessageService", () => {
+  let service: MessageService;
+
+  beforeEach(() => {
+
+  });
+
+  it("should have no messages to start with", () => {
+    service = new MessageService();
+
+    expect(service.messages.length).toBe(0);
+  });
+
+  it("should add a message when add is called", () => {
+    service = new MessageService();
+    service.add("new message");
+
+    expect(service.messages.length).toBe(1);
+  });
+
+  it("should remove all messages when clear is called", () => {
+    service = new MessageService();
+    service.add("new message");
+
+    service.clear();
+
+    expect(service.messages.length).toBe(0);
+  });
+});
--- src/app/strength/strength.pipe.spec.ts
+++ src/app/strength/strength.pipe.spec.ts
@@ -1,20 +1,19 @@
-import { StrengthPipe } from "./strength.pipe"
+import { StrengthPipe } from "./strength.pipe";
 
-describe('StrengthPipe', () => {
+describe("StrengthPipe", () => {
+  it("should display weak if strength is 5", () => {
+    let pipe = new StrengthPipe();
 
-    it('should display weak if strength is 5', () => {
-        let pipe = new StrengthPipe();
+    let val = pipe.transform(5);
 
-        let val = pipe.transform(5);
+    expect(val).toEqual("5 (weak)");
+  });
 
-        expect(val).toEqual('5 (weak)')
-    })
+  it("should display strong if strength is 10", () => {
+    let pipe = new StrengthPipe();
 
-    it('should display strong if strength is 10', () => {
-        let pipe = new StrengthPipe();
+    let val = pipe.transform(10);
 
-        let val = pipe.transform(10);
-
-        expect(val).toEqual('10 (strong)')
-    })
-})
+    expect(val).toEqual("10 (strong)");
+  });
+});
