6f4339ca4d83da4c1daa34a993c1e654aeb236f4
Oliver Baylis <oliver.baylis@willistowerswatson.com>
2020-10-27 09:52:50 +0000

    added simple shallow integration test

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -1,31 +1,30 @@
-import { Observable } from "rxjs";
-import { of } from "rxjs/internal/observable/of";
-import { Hero } from "../hero";
-import { HeroesComponent } from "./heroes.component";
+import { of } from 'rxjs/internal/observable/of';
+import { Hero } from '../hero';
+import { HeroesComponent } from './heroes.component';
 
-describe("HeroesComponent", () => {
+describe('HeroesComponent', () => {
   let component: HeroesComponent;
   let HEROES: Array<Hero>;
   let mockHeroService;
 
   beforeEach(() => {
     HEROES = [
-      { id: 1, name: "SpiderDude", strength: 8 },
-      { id: 2, name: "Wonderful Woman", strength: 24 },
-      { id: 3, name: "SuperDude", strength: 55 },
+      { id: 1, name: 'SpiderDude', strength: 8 },
+      { id: 2, name: 'Wonderful Woman', strength: 24 },
+      { id: 3, name: 'SuperDude', strength: 55 },
     ];
 
     mockHeroService = jasmine.createSpyObj([
-      "getHeroes",
-      "addHero",
-      "deleteHero",
+      'getHeroes',
+      'addHero',
+      'deleteHero',
     ]);
 
     component = new HeroesComponent(mockHeroService);
   });
 
-  describe("delete", () => {
-    it("should remove the indicated hero from the heroes list", () => {
+  describe('delete', () => {
+    it('should remove the indicated hero from the heroes list', () => {
       mockHeroService.deleteHero.and.returnValue(of(true)); // return simplest observable as heroeService doesn't care what is in the observable
       component.heroes = HEROES;
 
@@ -38,13 +37,13 @@ describe("HeroesComponent", () => {
       expect(deletedHero).toBeUndefined();
     });
 
-    it("should call deleteHero with correct value", () => {
+    it('should call deleteHero with correct value', () => {
       mockHeroService.deleteHero.and.returnValue(of(true)); // return simplest observable as heroeService doesn't care what is in the observable
       component.heroes = HEROES;
-      
+
       component.delete(HEROES[2]);
 
       expect(mockHeroService.deleteHero).toHaveBeenCalledOnceWith(HEROES[2]);
-    });    
+    });
   });
 });
